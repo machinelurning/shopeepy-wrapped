3e33afe4664e44b4805c90015e917abb236507db
IK Manding <github@ianmanding.net>
2023-02-15 09:49:38 +0800

    added test to main

--- shopeepy_wrapped/main.py
+++ shopeepy_wrapped/main.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 
-from login import login
+from shopeepy_wrapped.login import login
 from shopeepy_wrapped.browser_automation.driver_setup import driver
 from shopeepy_wrapped.browser_automation.element_locator import element_id_generator
 from shopeepy_wrapped.browser_automation.scroll import scroll_to_bottom
@@ -9,20 +9,22 @@ from shopeepy_wrapped.config.core import config
 from shopeepy_wrapped.scrapee import Order
 
 
-def main():
+def main(test=False):
     login()
 
     driver.get(config.scrapee_config.PURCHASEPAGE_LINK)
 
     webdriverwait(config.scrapee_config.PURCHASES)
 
-    scroll_to_bottom()
+    if not test:
+        scroll_to_bottom()
 
     soup = BeautifulSoup(driver.page_source, features="html.parser")
-    order_elements = soup.find_all(*element_id_generator(**config.scrapee_config.PURCHASES))
+    order_elements = soup.find_all(
+        *element_id_generator(**config.scrapee_config.PURCHASES)
+    )
 
-    orders = [Order(order_element).get_order_parameters() for order_element in order_elements]
+    orders = [
+        Order(order_element).get_order_parameters() for order_element in order_elements
+    ]
     return orders
-
-
-main()
