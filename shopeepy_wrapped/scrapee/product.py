33c4fee98e4c4351df96f128e723311691fded54
IK Manding <github@ianmanding.net>
2023-02-15 18:09:01 +0800

    handled some typechecking bugs

--- shopeepy_wrapped/scrapee/product.py
+++ shopeepy_wrapped/scrapee/product.py
@@ -1,55 +1,50 @@
 import numpy as np
-from bs4 import BeautifulSoup
-from selenium.common.exceptions import TimeoutException
 
-from shopeepy_wrapped.browser_automation.driver_setup import driver
 from shopeepy_wrapped.browser_automation.element_locator import element_id_generator
-from shopeepy_wrapped.browser_automation.webdriverwait import webdriverwait
 from shopeepy_wrapped.config.core import config
-from shopeepy_wrapped.href.href_manipulation import append_site_prefix
 
 
 class Product:
     def __init__(self, product_element):
         self.product_element = product_element
-        self.product_bread_crumb = None
+        # self.product_bread_crumb = None
         self.product_name = None
         self.product_price = None
 
-    def bread_crumb_string(self, bread_crumbs):
-        bread_crumb_levels = [
-            bread_crumb_level.text for bread_crumb_level in bread_crumbs
-        ]
-        return " > ".join(bread_crumb_levels)
-
-    def get_bread_crumbs(self, href):
-        driver.get(href)
-        try:
-            webdriverwait(config.scrapee_config.BREAD_CRUMB)
-            product_soup = BeautifulSoup(driver.page_source, features="html.parser")
-            bread_crumb_element = product_soup.find(
-                *element_id_generator(config.scrapee_config.BREAD_CRUMB_ELEMENT)
-            )
-
-            bread_crumbs = bread_crumb_element.find_all(
-                *element_id_generator(config.scrapee_config.BREAD_CRUMB)
-            )
-
-            return self.bread_crumb_string(bread_crumbs)
-
-        except TimeoutException:
-            print("Product does not exist anymore.")
-            return np.nan
-
-        except AttributeError:
-            return np.nan
+    # def bread_crumb_string(self, bread_crumbs):
+    #     bread_crumb_levels = [
+    #         bread_crumb_level.text for bread_crumb_level in bread_crumbs
+    #     ]
+    #     return " > ".join(bread_crumb_levels)
+    #
+    # def get_bread_crumbs(self, href):
+    #     driver.get(href)
+    #     try:
+    #         webdriverwait(config.scrapee_config.BREAD_CRUMB)
+    #         product_soup = BeautifulSoup(driver.page_source, features="html.parser")
+    #         bread_crumb_element = product_soup.find(
+    #             *element_id_generator(config.scrapee_config.BREAD_CRUMB_ELEMENT)
+    #         )
+    #
+    #         bread_crumbs = bread_crumb_element.find_all(
+    #             *element_id_generator(config.scrapee_config.BREAD_CRUMB)
+    #         )
+    #
+    #         return self.bread_crumb_string(bread_crumbs)
+    #
+    #     except TimeoutException:
+    #         print("Product does not exist anymore.")
+    #         return np.nan
+    #
+    #     except AttributeError:
+    #         return np.nan
 
     def get_product_names(self):
         try:
             name = self.product_element.find(
                 *element_id_generator(config.scrapee_config.NAME)
             )
-            return name.text
+            print(name.text)
         except AttributeError:
             return np.nan
 
@@ -64,6 +59,7 @@ class Product:
                 .strip()
             )
             return price
+
         except AttributeError:
             return np.nan
 
@@ -71,8 +67,8 @@ class Product:
         self.product_name = self.get_product_names()
         self.product_price = self.get_product_prices()
 
-        product_link = append_site_prefix(self.product_element["href"])
-        self.product_bread_crumb = self.get_bread_crumbs(product_link)
+        # product_link = append_site_prefix(self.product_element["href"])
+        # self.product_bread_crumb = self.get_bread_crumbs(product_link)
 
     def get_product_details(self):
         product_details = {}
