e0969e4371bc00aa35f89eefacb87cdce1dcfe52
IK Manding <github@ianmanding.net>
2023-02-17 11:52:02 +0800

    added parsing module

---
+++ shopeepy_wrapped/parsing/parse_orders.py
@@ -0,0 +1,16 @@
+from typing import Dict, Tuple, Any
+
+import pandas as pd
+
+
+def filter_products(order: Dict) -> Dict:
+    return {key: order[key] for key in order.keys() if key != "products"}
+
+
+def remove_products(orders: Tuple[Dict]) -> Tuple[Any, ...]:
+    return tuple(filter_products(order) for order in orders)
+
+
+def generate_orders_df(orders: Tuple[Dict]) -> pd.DataFrame:
+    orders_no_products = remove_products(orders=orders)
+    return pd.DataFrame(orders_no_products)
---
+++ shopeepy_wrapped/parsing/parse_products.py
@@ -0,0 +1,20 @@
+from typing import Tuple, Dict, List
+
+import pandas as pd
+
+
+def get_products(orders: Tuple[Dict]) -> List[Dict]:
+    products = []
+
+    for order in orders:
+        products_list = order["products"]
+        order_id = order["order_id"]
+        for product in products_list:
+            product["order_id"] = order_id
+            products.append(product)
+
+    return products
+
+
+def generate_products_df(orders: Tuple[Dict]) -> pd.DataFrame:
+    return pd.DataFrame(get_products(orders))
